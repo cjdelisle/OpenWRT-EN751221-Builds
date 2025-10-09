b701ea130c41b7d66b13943d460c6dbaa5a3b059
Caleb James DeLisle <cjd@cjdns.fr>
2025-10-09 13:32:44 +0000

    Trigger new build with nokia-g240g-e

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-git clone https://github.com/openwrt/openwrt.git
+git clone https://github.com/cjdelisle/openwrt.git
 cd openwrt || exit 1
-git checkout main
+git checkout econet-add-nokia-g240g-e-oct6-2025
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
@@ -11,17 +11,42 @@ echo '
 CONFIG_TARGET_econet=y
 CONFIG_TARGET_econet_en751221=y
 CONFIG_TARGET_MULTI_PROFILE=y
+CONFIG_TARGET_DEVICE_econet_en751221_DEVICE_en751221_generic=y
+CONFIG_TARGET_DEVICE_PACKAGES_econet_en751221_DEVICE_en751221_generic=""
+CONFIG_TARGET_DEVICE_econet_en751221_DEVICE_nokia_g240g-e=y
+CONFIG_TARGET_DEVICE_PACKAGES_econet_en751221_DEVICE_nokia_g240g-e=""
 CONFIG_TARGET_DEVICE_econet_en751221_DEVICE_smartfiber_xp8421-b=y
+CONFIG_TARGET_DEVICE_PACKAGES_econet_en751221_DEVICE_smartfiber_xp8421-b=""
 CONFIG_TARGET_DEVICE_econet_en751221_DEVICE_tplink_archer-vr1200v-v2=y
+CONFIG_TARGET_DEVICE_PACKAGES_econet_en751221_DEVICE_tplink_archer-vr1200v-v2=""
+CONFIG_TARGET_PER_DEVICE_ROOTFS=y
+CONFIG_FEED_luci=y
+CONFIG_FEED_packages=y
+CONFIG_FEED_routing=y
+CONFIG_FEED_telephony=y
+CONFIG_FEED_video=y
+CONFIG_IMAGEOPT=y
+CONFIG_PACKAGE_kmod-crypto-sha256=y
+CONFIG_PACKAGE_kmod-fs-ext4=y
+CONFIG_PACKAGE_kmod-fs-vfat=y
+CONFIG_PACKAGE_kmod-lib-crc16=y
+CONFIG_PACKAGE_kmod-libphy=y
+CONFIG_PACKAGE_kmod-mii=y
+CONFIG_PACKAGE_kmod-nls-cp437=y
+CONFIG_PACKAGE_kmod-nls-iso8859-1=y
+CONFIG_PACKAGE_kmod-nls-utf8=y
+CONFIG_PACKAGE_kmod-scsi-core=y
+CONFIG_PACKAGE_kmod-tun=y
+CONFIG_PACKAGE_kmod-usb-net-rtl8152=y
+CONFIG_PACKAGE_kmod-usb-storage=y
 CONFIG_PACKAGE_nand-utils=y
+CONFIG_PACKAGE_libatomic=y
+CONFIG_PACKAGE_libpthread=y
+CONFIG_PACKAGE_librt=y
+CONFIG_PACKAGE_libstdcpp=y
+CONFIG_PACKAGE_r8152-firmware=y
 CONFIG_TARGET_INITRAMFS_COMPRESSION_NONE=y
 CONFIG_TARGET_ROOTFS_INITRAMFS=y
-CONFIG_PACKAGE_kmod-usb-storage=y
-CONFIG_PACKAGE_kmod-usb-net-rtl8152=y
-CONFIG_PACKAGE_kmod-fs-vfat=y
-CONFIG_PACKAGE_kmod-fs-ext4=y
-CONFIG_PACKAGE_kmod-scsi-core=y
-CONFIG_PACKAGE_r8152-firmware=y
 ' > .config
 
 make defconfig
